a5f4b8116d1c7c05cc7ab4d789b1214cab339039
Alan Ravy <39372639+Machinecoach@users.noreply.github.com>
2019-04-27 06:16:49 -0400

    Update lambdas_start.py

--- Ex_Files_Adv_Python/Exercise Files/03 Functions/lambdas_start.py
+++ Ex_Files_Adv_Python/Exercise Files/03 Functions/lambdas_start.py
@@ -14,8 +14,12 @@ def main():
     ftemps = [32, 65, 100, 212]
 
     # TODO: Use regular functions to convert temps
+    print(list(map(FahrenheitToCelsisus, ftemps)))
+    print(list(map(CelsisusToFahrenheit, ctemps)))
 
-    # TODO: Use lambdas to accomplish the same thing
+    # TODO: Use lambdas to accomplish the same thing (this helps developers see what calc a Func is doing in place
+    print(list(map(lambda t: (t-32) * 5/9, ftemps)))
+    print(list(map(lambda t: (t * 9/5) + 32, ctemps)))
 
 
 if __name__ == "__main__":
